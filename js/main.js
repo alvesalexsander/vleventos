8fccdfdad84135529e5bbc28e7cbcdb439ffe978
sashaclimax <wolfiganlex15@hotmail.com>
2017-09-11 10:15:43 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 1500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 6000,          // Integer: Time between slide transitions, in milliseconds
+});
