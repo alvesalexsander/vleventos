2763871595c4356d524f6667c055877d17090828
sashaclimax <alvesalexsander@live.com>
2017-10-01 19:39:22 -0300

    Implementação menu dropdown mobile
    Érika e eu adicionamos um menu dropdown para a versão mobile do site *O*
    
    Foi nossa primeira implementação JS juntinhos heheheh

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ $(".rslides").responsiveSlides({
     speed: 1500,            // Integer: Speed of the transition, in milliseconds
     timeout: 6000,          // Integer: Time between slide transitions, in milliseconds
 });
+
+function menu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
